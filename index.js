89a1975c4ef92156eea3ae6bebefdf7e37ddc2d9
Sasa Savic Perusina <sasa@bubulazy.com>
2022-04-20 14:14:11 +0100

    Diseño de la pagina y el CSS terminados.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fetch("https://api.eva.pingutil.com/email?email=support@hexlox.com")
-.then(response => response.text())
-.then(result => console.log(result))
-.catch(error => console.log('error', error));
+// fetch("https://api.eva.pingutil.com/email?email=support@hexlox.com")
+// .then(response => response.text())
+// .then(result => console.log(result))
+// .catch(error => console.log('error', error));
