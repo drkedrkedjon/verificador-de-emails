1e5df705343d92907277806436142465d26047b5
Sasa Savic Perusina <sasa@bubulazy.com>
2022-04-20 18:57:24 +0100

    Implementacion de la funcionalidad basica en JS

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-// fetch("https://api.eva.pingutil.com/email?email=support@hexlox.com")
-// .then(response => response.text())
-// .then(result => console.log(result))
-// .catch(error => console.log('error', error));
-
+const paraffo = document.querySelector('#mensaje')
 const btnVerificar = document.querySelector('#btn-verificar')
 const txtArea = document.querySelector('#txt-area')
 btnVerificar.addEventListener('click', verificador)
 
 function verificador() {
-  console.log(txtArea.value)
+  fetch(`https://api.eva.pingutil.com/email?email=${txtArea.value}`)
+  .then(response => response.json())
+  .then(data => {
+    let spamMensaje = ''
+    let deliverableMensaje = ''
+    // if (data.data.spam) {
+    //   spamMensaje = 'MENSAJE SUPER'
+    // } else {
+    //   spamMensaje = 'MIERDA MENSAJE'
+    // }
+    data.data.spam ? spamMensaje = 'MENSAJE SUPER' : spamMensaje = 'MIERDA MENSAJE'
+    data.data.deliverable ? deliverableMensaje = 'TRUE' : deliverableMensaje = 'FALSE'
+
+    paraffo.innerHTML = `
+    <p>El correo electronico: ${data.data.email_address}, proviene de dominio: ${data.data.domain}.</br>
+    El correo es entregable: ${data.data.deliverable}, y esta ${spamMensaje} marcado ${deliverableMensaje} como el spam: ${data.data.spam} </p>
+    `
+    // .catch(error => console.log('error', error));
+    }
+  )
 }
-
-
-
-
-
-
