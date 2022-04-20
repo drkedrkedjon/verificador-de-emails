4aae7560c9ba6084a739052ab20fc95950f4a0dc
Sasa Savic Perusina <sasa@bubulazy.com>
2022-04-20 14:32:54 +0100

    Agarando elementos en DOM y añadiendo event listener

--- index.js
+++ index.js
@@ -2,3 +2,17 @@
 // .then(response => response.text())
 // .then(result => console.log(result))
 // .catch(error => console.log('error', error));
+
+const btnVerificar = document.querySelector('#btn-verificar')
+const txtArea = document.querySelector('#txt-area')
+btnVerificar.addEventListener('click', verificador)
+
+function verificador() {
+  console.log(txtArea.value)
+}
+
+
+
+
+
+
