5a515125141c544e8053bc3b487cf63eb35ebffc
Sasa Savic Perusina <sasa@bubulazy.com>
2022-04-21 12:18:49 +0100

    Refactorar la function para usar async await

--- index.js
+++ index.js
@@ -5,22 +5,23 @@ btnVerificar.addEventListener('click', verificador)
 let spamMensaje = ''
 let deliverableMensaje = ''
 
-function verificador() {
-  fetch(`https://api.eva.pingutil.com/email?email=${txtArea.value}`)
-  .then(res => {
-    if(!res.ok) {
-      throw new Error('Ha ocurido una KAKITA')
-    } return res.json()
-  })
-  .then(data => {
-    // Cabiar mensaje
-    data.data.spam ? spamMensaje = 'MENSAJE SUPER' : spamMensaje = 'MIERDA MENSAJE'
-    data.data.deliverable ? deliverableMensaje = 'TRUE' : deliverableMensaje = 'FALSE'
+async function verificador() {
+  try {
+    const respuesta = await fetch(`https://api.eva.pingutil.com/email?email=${txtArea.value}`)
+      if (!respuesta.ok) {
+        paraffo.innerHTML = `Ha ocurido un error en el servidor, intentalo mas tarde`
+        throw new Error('Ha ocurido una KAKITA')
+      }
+    const data = await respuesta.json()
+
+    data.data.spam ? spamMensaje = 'ES SPAM' : spamMensaje = 'NO ES SPAM'
+    data.data.deliverable ? deliverableMensaje = 'ES ENTREGABLE' : deliverableMensaje = 'NO ES ENTREGABLE'
 
     paraffo.innerHTML = `
-    <p>El correo electronico: ${data.data.email_address}, proviene de dominio: ${data.data.domain}.</br>
-    El correo es entregable: ${data.data.deliverable}, y esta ${spamMensaje} marcado ${deliverableMensaje} como el spam: ${data.data.spam} </p>
+      <p>Este correo electronico (${data.data.email_address}) proviene de dominio (${data.data.domain}).</br>
+      El correo ${deliverableMensaje}, y ${spamMensaje}</p>    
     `
-  })
-  .catch(error => console.error(error));
+  } catch (fallo) {
+    console.error(fallo)
+  }
 }
