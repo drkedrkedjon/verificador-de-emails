60a2cb191d7cd5fbc90405f73c58177a797e91ac
Sasa Savic Perusina <sasa@bubulazy.com>
2022-04-21 12:47:41 +0100

    Fallos ortograficos

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ async function verificador() {
   try {
     const respuesta = await fetch(`https://api.eva.pingutil.com/email?email=${txtArea.value}`)
       if (!respuesta.ok) {
-        paraffo.innerHTML = `Ha ocurido un error en el servidor, intentalo mas tarde`
+        paraffo.innerHTML = `Ha ocurido un error en el servidor, inténtalo más tarde`
         throw new Error('Ha ocurido una KAKITA')
       }
     const data = await respuesta.json()
@@ -18,7 +18,7 @@ async function verificador() {
     data.data.deliverable ? deliverableMensaje = 'ES ENTREGABLE' : deliverableMensaje = 'NO ES ENTREGABLE'
 
     paraffo.innerHTML = `
-      <p>Este correo electronico (${data.data.email_address}) proviene de dominio (${data.data.domain}).</br>
+      <p>Este correo electrónico (${data.data.email_address}) proviene del dominio (${data.data.domain}).</br>
       El correo ${deliverableMensaje}, y ${spamMensaje}</p>    
     `
   } catch (fallo) {
